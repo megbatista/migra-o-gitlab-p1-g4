2d1a9535fa9b017b91e5c33a99e40ad6ec0a20be
Patrick Dantas <patrickdsilva99@gmail.com>
2017-09-07 20:29:36 +0000

    Modificações: Introdução da função privmsg. Fiz a busca somente para Nick (não funciona com o socket.name) Função analisar: modifiquei o último else. Coloquei para o broadcast exibir o nick do usuário se ele possuir um nick. No último else, a mensagem tem que ser tratada como uma mensagem comum por não ter nenhum argumento reconhecido como comando.

--- server/irc-server.js
+++ server/irc-server.js
@@ -8,7 +8,8 @@ var clients = [];
 var nicks = {};
 
 // Start a TCP Server
-net.createServer(function (socket) {
+net.createServer(function (socket) 
+{
 
   // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
@@ -21,19 +22,23 @@ net.createServer(function (socket) {
   broadcast(socket.name + " joined the chat\n", socket);
 
   // Handle incoming messages from clients.
-  socket.on('data', function (data) {
+  socket.on('data', function (data) 
+  {
     analisar(data);
   });
 
   // Remove the client from the list when it leaves
-  socket.on('end', function () {
+  socket.on('end', function () 
+  {
     clients.splice(clients.indexOf(socket), 1);
     broadcast(socket.name + " left the chat.\n");
   });
   
   // Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
+  function broadcast(message, sender)
+  {
+    clients.forEach(function (client) 
+    {
       // Don't want to send it to sender
       if (client === sender) return;
       client.write(message);
@@ -42,31 +47,77 @@ net.createServer(function (socket) {
     process.stdout.write(message)
   }
 
-function analisar(data){
+function analisar(data)
+{
   let mensagem = String(data).trim();
   let args = mensagem.split(" ");
 
   if(args[0] == 'NICK') nick(args);
   else if(args[0] == 'USER') user(args);
   else if(args[0] == 'JOIN') join(args);
-  else socket.write("ERRO: comando inexistente");
+  else if(args[0] == 'PRIVMSG') privmsg(args);
+  else
+  {
+    if(!socket.nick) broadcast(socket.name + " > " + data,socket);
+    else broadcast(socket.nick + " > " + data,socket);
+  }
 }
 
-function nick(args){
-  if(!args[1]){
-    socket.write('ERRO: nick faltando');
+function nick(args)
+{
+  if(!args[1])
+  {
+    socket.write('ERRO: nick faltando\n');
     return;
   }
-  else if(nicks[args[1]]){
-    socket.write('ERRO: o nick informado ja existe');
-  }else{
-    if(socket.nick){
+  else if(nicks[args[1]])
+  {
+    socket.write('ERRO: o nick informado ja existe\n');
+  }
+  else
+  {
+    if(socket.nick)
+    {
       delete nicks[socket.nick];
     }
     // associa ao atributo nick informado o valor: nome do socket(ip:porta);
     nicks[args[1]] = socket.name;
     socket.nick = args[1];
   }
+}
+
+function privmsg(args)
+{
+    if(!args[1])
+    {
+        socket.write("ERRO: nick não fornecido\n");
+        return;
+    }
+    else if(nicks[args[1]])
+    {
+        clients.forEach(function(client)
+        {
+            if(client.nick === args[1]) 
+            {
+                client.write(socket.nick + " > ");
+                
+                for (var i=2 ; i<args.length ; i++)
+                {
+                    client.write(args[i]+" ");
+                }
+                
+                client.write("\n");
+            }
+        });
+        
+        socket.write("Comando PRIV_MSG executado com sucesso!\n");
+    }
+    else
+    {
+        socket.write("ERRO: nick não encontrado!\n");
+        return;
+    }
+    
 }
 function join(args){
   socket.write('Comando JOIN a ser implementado');
