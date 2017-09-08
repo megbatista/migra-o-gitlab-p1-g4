785fc755501c049c8135a09c112d4454e5d566b3
vitor <vitorafaelgr@gmail.com>
2017-09-08 00:46:43 -0300

    Implementação da função JOIN, com a função PRINT para fazer testes

--- server/irc-server.js
+++ server/irc-server.js
@@ -1,17 +1,21 @@
 // Load the TCP Library
 var net = require('net');
 
+//Carrega os modulos em suas respectivas variaveis
+var join = require('../comandos/join.js');
 // Keep track of the chat clients
 var clients = [];
 
 //Cria um objeto pra armazenar nicks
 var nicks = {};
+//Cria um array para armazenar os canais
+var canais = [];
 
 // Start a TCP Server
 net.createServer(function (socket) {
 
   // Identify this client
-  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+  socket.name = socket.remoteAddress + ":" + socket.remotePort
 
   // Put this new client in the list
   clients.push(socket);
@@ -30,7 +34,7 @@ net.createServer(function (socket) {
     clients.splice(clients.indexOf(socket), 1);
     broadcast(socket.name + " left the chat.\n");
   });
-  
+
   // Send a message to all clients
   function broadcast(message, sender) {
     clients.forEach(function (client) {
@@ -48,7 +52,8 @@ function analisar(data){
 
   if(args[0] == 'NICK') nick(args);
   else if(args[0] == 'USER') user(args);
-  else if(args[0] == 'JOIN') join(args);
+  else if(args[0] == 'JOIN') join(args, canais, socket);
+  else if(args[0] == 'PRINT') print(canais);
   else socket.write("ERRO: comando inexistente");
 }
 
@@ -68,14 +73,22 @@ function nick(args){
     socket.nick = args[1];
   }
 }
-function join(args){
-  socket.write('Comando JOIN a ser implementado');
-}
 
 function user(args){
   socket.write('Comando USER a ser implementado');
 }
 
+function print(vetorDeCanais)
+{
+  for(canal in vetorDeCanais)
+  {
+    socket.write(vetorDeCanais[canal].nomeDoCanal + '\n');
+    for(usuario in vetorDeCanais[canal].usuarios)
+    {
+      socket.write(' ' + vetorDeCanais[canal].usuarios[usuario].nick + '\n');
+    }
+  }
+}
 }).listen(6667);
 
 
